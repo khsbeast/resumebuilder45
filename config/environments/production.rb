e60c683fac47d99796c6adb9c41598434f980d48
khsbeast99 <harsh4981@gmail.com>
2022-01-10 02:31:50 +0530

    added aws-reg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,16 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-  
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_region => ENV['AWS_REGION'],
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+}
+
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
