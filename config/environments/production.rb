df9f85624249b7825a544bf855ec9861ceccd870
khsbeast99 <harsh4981@gmail.com>
2022-01-10 02:41:16 +0530

    added aws-regs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,12 +40,16 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
   config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_region => ENV['AWS_REGION'],
-      :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}}
+    storage: :s3,
+    s3_host_name: "s3-us-west-2.amazonaws.com",
+    s3_region: ENV['AWS_REGION']
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    }
+  }
+  
 
 
 
