12e61b3620875a40b1df6279422e9d4b9227d5c3
khsbeast99 <harsh4981@gmail.com>
2022-01-10 03:39:04 +0530

    added aws-regssddssd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,14 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
 
-  
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_region => ENV['AWS_REGION'],
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+}}
 
 
 
