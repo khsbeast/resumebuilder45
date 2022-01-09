cf04de8b168a1299045609ca7d9d150b5f2db5d7
khsbeast99 <harsh4981@gmail.com>
2022-01-10 03:57:59 +0530

    added aws-regssddssdsss

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,6 +3,5 @@ Aws.config[:credentials] = Aws::Credentials.new(
   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
 )
 Aws.config[:region]      = 'us-east-1'
-Aws.config[:name]        = 's3-us-east-resumebuilder'
 
 S3 = Aws::S3::Client.new
