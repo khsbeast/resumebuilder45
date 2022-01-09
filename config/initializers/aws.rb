fc02ba570f47e85dbae399fc31b32335fb419bab
khsbeast99 <harsh4981@gmail.com>
2022-01-10 04:12:55 +0530

    added aws-regssddssdssssssss

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
-CarrierWave.configure do |config|
-    config.aws_bucket =  ENV['AWS_BUCKET']
-    config.aws_credentials = {
-      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:            'us-west-1'
-    }
-  end
+Aws.config[:credentials] = Aws::Credentials.new(
+  ENV['AWS_BUCKET'],
+  ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+  ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+)
+Aws.config[:region]      = 'us-east-1'
+
+S3 = Aws::S3::Client.new
