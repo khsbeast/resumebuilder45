719336ab557701f98e5e7525683ad626319d493b
khsbeast99 <harsh4981@gmail.com>
2022-01-10 06:03:44 +0530

    make it better

---
+++ Changelog.md
@@ -0,0 +1,18 @@
+# Flexiple Assignment
+
+## Tech stack for this app
+
+- RoR
+- PostgreSQL
+- tailwindcss
+
+## Tasks Assigned
+
+# User sign up Status:Completed 
+
+Files Added/Modified :- config/routes.db(ADD signup route), controller/users_controller.db , model/user.rb , views/users
+
+# Change profile image Status:Completed 
+
+Files Added/Modified :- Active_storage(To_store_file)config/routes.db(ADD signup route), controller/users_controller.db , model/user.rb , views/users
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
-  resources :profiles , only: [:update,:show]   #Profile routes to display and update
-  resources :educations , only: [:new]          
-  resources :experiences , only: [:new]         #Create new experience entry api route
-  resources :projects , only: [:new]            #Create new project entry api route
- 
+  resources :profiles , only: [:update,:show]
+  resources :educations , only: [:new]
+  resources :experiences , only: [:new]
+  resources :projects , only: [:new]
+  
 end
